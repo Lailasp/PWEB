ec674d5ab60c7885cae08fbf487e0c456b49c411
Lailasp <lbsp1@aluno.ifal.edu.br>
2025-09-18 07:58:23 -0300

    Create Biblioteca.ts

---
+++ Atv_biblioteca/Biblioteca.ts
@@ -0,0 +1,41 @@
+// Biblioteca.ts
+
+import { IBiblioteca } from './IBiblioteca';
+
+export class Biblioteca {
+  livros: IBiblioteca[] = [];
+
+  adicionarLivro(livro: IBiblioteca): void {
+    this.livros.push(livro);
+    console.log(`Livro "${livro.titulo}" adicionado à biblioteca.`);
+  }
+
+  excluirLivro(isbn: number): void {
+    const livroIndex = this.livros.findIndex(livro => livro.isbn === isbn);
+    if (livroIndex > -1) {
+      const livroRemovido = this.livros.splice(livroIndex, 1);
+      console.log(`Livro "${livroRemovido[0].titulo}" removido da biblioteca.`);
+    } else {
+      console.log(`Livro com ISBN ${isbn} não encontrado.`);
+    }
+  }
+
+  venderLivro(isbn: number): void {
+    const livro = this.livros.find(livro => livro.isbn === isbn);
+    if (livro && livro.estoque > 0) {
+      livro.atualizarEstoque(-1); // Reduz o estoque em 1
+      console.log(`Livro "${livro.titulo}" vendido! Estoque atualizado: ${livro.estoque}`);
+    } else {
+      console.log(`Livro com ISBN ${isbn} não disponível em estoque.`);
+    }
+  }
+
+  exibirLivros(): void {
+    if (this.livros.length === 0) {
+      console.log('Nenhum livro na biblioteca.');
+    } else {
+      console.log('Livros na biblioteca:');
+      this.livros.forEach(livro => livro.exibirLivro());
+    }
+  }
+}
